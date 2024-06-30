a9139449017d7b1035c3119971bd3c6981e18f05
Dalila K Leo <dalilakatialeo@gmail.com>
2024-06-30 12:30:34 +1000

    feat: add logo, favicon and adjust landing page coming soon

---
+++ tempotrot/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to TempoTrot!</p>
+        <p>🚧 Coming soon... 🚧</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
